9cdcb6130818556a27f1947c77e40a861ae31f39
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-14 20:40:17 +0530

    [Chetan] Add: This script is used to check Employees attendence.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_CheckAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("--------------------------------------------");
+
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+
+            //Checking if the Employee is Present or not
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
