569065770ede6f39008674ec455955a8273a44bc
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-14 20:55:07 +0530

    [Chetan] Add: This script is used to Calculate Employees Daily Wages.

--- Program.cs
+++ Program.cs
@@ -10,20 +10,43 @@ namespace UC1_CheckAttendance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("Employee Wage Computation Program");
             Console.WriteLine("--------------------------------------------");
+
+            //Random random = new Random();
+            //int attendance = random.Next(0, 2);
+
+            ////Checking if the Employee is Present or Absent.
+
+            //if (attendance == 1)
+            //{
+            //    Console.WriteLine("Employee is Present.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee is Absent.");
+            //}
+          
+
 
             Random random = new Random();
             int attendance = random.Next(0, 2);
 
-            //Checking if the Employee is Present or not
+            int Salary = 0;
+
+            //Constant variables
+            const byte FULL_DAY_HOUR = 8;
+            const byte WAGE_PER_HOUR = 20;
+
+            //Calculate Employee Present Salary and Absent Salary.
             if (attendance == 1)
             {
-                Console.WriteLine("Employee is Present");
+                Salary = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine("Employee is Present for Full time and the Salary is Rs: " + Salary);
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is Absent and the Salary is Rs: " + Salary);
             }
             Console.ReadLine();
         }
