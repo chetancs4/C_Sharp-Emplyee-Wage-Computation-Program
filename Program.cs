f5284133d55519f42b16121d9b94126bd09c8dc2
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-14 21:41:33 +0530

    [Chetan] Add: This script is used to Calculate Employees WagesUsing Switch Case.

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace UC1_CheckAttendance
              }*/
 
 
-            Random random = new Random();
+            /*Random random = new Random();
             int attendance = random.Next(0, 3);
 
             int salary = 0;
@@ -77,7 +77,36 @@ namespace UC1_CheckAttendance
             else
             {
                 Console.WriteLine("Employee is Absent and the Salary is Rs." + salary);
+            }*/
+
+            //Using Switch Case
+            Random random = new Random();
+            int attendance = random.Next(0, 3);
+            int Salary = 0, daily_hour = 0;
+
+            //Constant variables
+            const byte IS_PRESENT_FULL_TIME = 1;
+            const byte IS_PRESENT_PART_TIME = 2;
+            const byte WAGE_PER_HOUR = 20;
+
+            switch (attendance)
+            {
+                case IS_PRESENT_FULL_TIME:
+                    daily_hour = 8;
+                    Console.WriteLine("Employee is Present for Full time.");
+                    break;
+
+                case IS_PRESENT_PART_TIME:
+                    daily_hour = 4;
+                    Console.WriteLine("Employee is Present for Part time.");
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent.");
+                    break;
             }
+            Salary = daily_hour * WAGE_PER_HOUR;
+            Console.WriteLine("Salary of the Employee is Rs: " + Salary);
             Console.ReadLine();
         }
     }
